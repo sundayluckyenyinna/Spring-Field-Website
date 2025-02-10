c439086eb28d4fa95f654a7a451dade349f5bbee
Sunday Lucky Enyinna <sundayluckyenyinna@gmail.com>
2025-02-10 14:05:19 +0100

    feat: finished email

--- src/main/java/com/springfield/website/modules/generic/email/EmailServiceImpl.java
+++ src/main/java/com/springfield/website/modules/generic/email/EmailServiceImpl.java
@@ -56,7 +56,7 @@ public class EmailServiceImpl implements EmailService{
             Properties props = mailSender.getJavaMailProperties();
             props.put("mail.transport.protocol", "smtp");
             props.put("mail.smtp.auth", "true");
-            props.put("mail.smtp.starttls.enable", "false");
+            props.put("mail.smtp.starttls.enable", "true");
             props.put("mail.smtp.ssl.enable", "true");
             props.put("mail.debug", "true");
 //            props.put("mail.smtp.ssl.trust", "smtp.zeptomail.com");
